70789610155b0764252dca56e508a2731c820ec2
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2020-05-03 15:26:14 -0500

    Edición 2. ver 2020.05-05 - Actualización de ML [+IQTree].

---
+++ ml/auto/ml.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "ml"
+ (lambda ()
+   (LaTeX-add-labels
+    "ch:likelihood"
+    "itm:model"
+    "itm:calc"
+    "itm:model1"
+    "itm:phyml")
+   (LaTeX-add-index-entries
+    "Likelihood!b\\'usquedas"))
+ :latex)
+
